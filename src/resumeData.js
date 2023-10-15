03f1c3ad5bc26453eb15a58c6ebb01e5d396af78
saisrushik <saisrushik98@gmail.com>
2023-10-15 22:21:05 +0530

    first commit

---
+++ src/resumeData.js
@@ -0,0 +1,114 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Sai Srushik",
+    "role": "Full-stack Developer and ML Engineer",
+    "linkedinId":"Sai srushik",
+    "emailId":"saisrushik98@gmail.com",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, read technical articles and research papers or simply watch movies in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/sai-srushik/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/saisrushik",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently a pre-final year student at The VIT-AP University and pursuing my B.Tech from here. I am a Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Hyderabad, India",
+    "education":[
+      {
+        "UniversityName":"VIT-AP University",
+        "specialization":"CSE-AI&ML",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2025",
+        "Achievements":"None"
+      },
+      {
+        "UniversityName":"NXTWAVE",
+        "specialization":"Full-stack Development",
+        "MonthOfPassing":"NA",
+        "YearOfPassing":"NA",
+        "Achievements":"Achieved 3-Month Consistent Learner"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Verzeo",
+        "specialization":"Data Science Intern",
+        "MonthOfLeaving":"July",
+        "YearOfLeaving":"2022",
+        "Achievements":"Certificate Of Internship"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Nodejs"
+      },
+      {
+        "skillname":"Python"
+      },
+      {
+        "skillname":"Java"
+      },
+      {
+        "skillname":"Machine-Learning"
+      },
+      {
+        "skillname":"Data-Visualization"
+      },
+      {
+        "skillname":"Tensorflow"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"Pneumonia Disease Prediction Using VGG19",
+        "description":"Deep Learning based Pneumonia Disease Prediction Model ",
+        "imgurl":"https://my.clevelandclinic.org/-/scassets/images/org/health/articles//21954-aspiration-pneumonia.jpg"
+      },
+      {
+        "name":"Wikipedia Application",
+        "description":"A fontend application developed using api fetch.",
+        "imgurl":"https://upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png"
+      },
+      {
+        "name":"Disease Diagonisis Using Human Nails",
+        "description":"Early disease prediction model based on the Human nails",  
+        "imgurl":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwtt5XuYLNrWJwRLzrHeTCxdedIp0QJKkr2Q&usqp=CAU"
+      },
+      {
+        "name":"Twitter API clone",
+        "description":"A NodeJs Application that performs the all the Twitter API'S",
+        "imgurl":"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/220px-Logo_of_Twitter.svg.png"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
